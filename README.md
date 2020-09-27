752d8feb214fdcb5fb9ca3c48be7cb9492b34087
gkoberger <gkoberger@gmail.com>
2020-09-26 18:32:58 -0700

    First attempt, but socket stuff feels too hard

--- README.md
+++ README.md
@@ -2,12 +2,30 @@ This will let you turn any Dropbox Paper doc into a teleprompter. You can contro
 
 If you see this and want to use it, create an Issue and I can fix the code up a bit so anyone can use it :)
 
-# Make it persist
+# Installing locally
 
-If you want this to work across app restarts, you'll need an `.env` file that contains the following Mongo connection details:
+```
+$ git clone git@github.com:gkoberger/teleprompter.git
+$ cd teleprompter
+$ npm install
+$ npm start
+```
+
+# Use a Database
+
+Right now, it saves the Paper doc in memory. If you want this to work across app restarts, you'll need an `.env` file that contains the following Mongo connection details:
 
 ```
 DB_USER=...
 DB_PASS=...
 DB_URL=...
 ```
+
+# Multi-user
+
+By default, this works for one person at a time. If you want it to be multi-user (useful for if you're letting lots of people use this, like the demo version), add this to your `.env` file:
+
+```
+MULTI=true
+```
+
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,49 @@ const _ = require('lodash');
 
 let teleCache = {
   title: 'Example Teleprompter',
-  markdown: 'Enter in a **Dropbox Paper** URL to see this in action!\n\nThe text will show up here. You can move the highlighted text using arrow keys.',
+  markdown:
+    'Enter in a **Dropbox Paper** URL to see this in action!\n\nThe text will show up here. You can move the highlighted text using arrow keys.',
 };
 
 router.use((req, res, next) => {
+  req.multi = process.env.MULTI === 'true';
+
   if (req.db) {
     req.tele = req.db.collection('tele');
+  } else {
+    if (req.multi) {
+      console.log("You need a DB for multi mode!");
+      process.exit(1);
+    }
   }
+
   next();
+});
+
+router.post('/setup', (req, res, next) => {
+  if (!req.body.email) return res.send('/');
+  if (!req.body.email.match(/@/)) return res.send('/');
+
+  res.cookie('email', req.body.email, { maxAge: 900000, httpOnly: true });
+  res.redirect('/');
+});
+
+router.use((req, res, next) => {
+  if (req.multi && !req.cookies.email) {
+    return res.render('setup');
+  }
+
+  if (req.cookies.email === 'single') {
+    res.clearCookie('email');
+    return res.redirect('/');
+  }
+
+  next();
+});
+
+router.get('/logout', (req, res, next) => {
+  res.clearCookie('email');
+  return res.redirect('/');
 });
 
 function loadFile(file, cb) {
@@ -31,18 +66,24 @@ function clean(text) {
 }
 
 function convert(tele) {
-  tele.markdown = tele.markdown.replace(/\[([-_\w]*):([-_\w]*)\]/g, '<span class="command">$1:$2</span>');
+  if (!tele.markdown) tele.markdown = '';
+  tele.markdown = tele.markdown.replace(
+    /\[([-_\w]*):([-_\w]*)\]/g,
+    '<span class="command">$1:$2</span>',
+  );
   return tele;
 }
 
 router.get('/', (req, res, next) => {
   if (req.db) {
     req.tele
-      .find({})
+      .find({
+        email: req.multi ? req.cookies.email : 'single',
+      })
       .sort({ created: -1 })
       .limit(1)
       .toArray((err, prompts) => {
-        const tele = convert(prompts[0]);
+        const tele = convert(prompts[0] || teleCache);
         res.render('index', { title: tele.title, tele, marked });
       });
   } else {
@@ -55,7 +96,7 @@ router.post('/', (req, res, next) => {
   const paper = req.body.url;
 
   if (!paper.match(/paper.dropbox/)) {
-    return res.send("You need a valid Dropbox Paper URL!");
+    return res.send('You need a valid Dropbox Paper URL!');
   }
 
   loadFile(paper, function (html) {
@@ -68,15 +109,17 @@ router.post('/', (req, res, next) => {
     var all = turndownService.turndown($('.ace-editor').html());
     */
     var json = JSON.parse($('#client-vars-json-tag').html());
-    var pad = _.find(Object.keys(json.initialDataStoreState.data), (i) => i.match(/pad/));;
+    var pad = _.find(Object.keys(json.initialDataStoreState.data), i =>
+      i.match(/pad/),
+    );
 
-    var all = json.initialDataStoreState.data[pad].collab_client_vars.collabBaseState.zatext.text['0'];
+    var all =
+      json.initialDataStoreState.data[pad].collab_client_vars.collabBaseState
+        .zatext.text['0'];
 
     all = all.replace(/[\n\r]+/g, '\n');
     all = all.replace(/\n\*+/g, '\n# ');
-    all = all.split(/\n/);
-
-    console.log(all);
+    all = all.trim().split(/\n/);
 
     var title = clean(all.shift().trim());
     var markdown = clean(all.join('\n\n'));
@@ -88,6 +131,7 @@ router.post('/', (req, res, next) => {
       url: req.body.url,
       title,
       markdown,
+      email: req.multi ? req.cookies.email : 'single',
     };
 
     if (req.db) {
